86188a8502d2d919bd891003ba6860601ce0a89f
russparsloe <russell.parsloe@4thlevelit.co.uk>
2015-04-23 19:19:20 +0100

    Updated Puppet agent install to support Puppet 4.0

--- install-procedures/3_install_puppet_agent.sh
+++ install-procedures/3_install_puppet_agent.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 #===============================================================================================#
 # Installs Puppet agent																			#
-#																								#
-# v0.1 - 26/03/2015 - RP - 	Initial release														#
-# v0.2 - 27/03/2015 - RP -	Made script more generic.											#
-#							Install specific Puppet agent version.								#
-#							Use Puppet commands where possible for broader OS compatibility		#
-#							Allow setting of PuppetMaster (defaults to ga-puppet if left blank)	#
 #===============================================================================================#
 
 # Set a location to be used for temporary files and logs
@@ -25,8 +19,8 @@ exec >> $TMPDIR/install-$(date +"%F-%H%M%S").log 2>&1
 
 # Download and install package to add Puppet repository
 echo "***LOG:*** Downloading Puppet repository package"
-wget -P $TMPDIR https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-sudo dpkg -i $TMPDIR/puppetlabs-release-trusty.deb
+wget -P $TMPDIR http://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
+sudo dpkg -i $TMPDIR/puppetlabs-release-pc1-trusty.deb
 
 # Refresh repositories
 echo "***LOG:*** Refreshing repositories"
@@ -34,7 +28,7 @@ sudo apt-get update
 
 # Install Puppet
 echo "***LOG:*** Installing Puppet agent"
-sudo apt-get -y install puppet=3.7.5-1puppetlabs1
+sudo apt-get -y install puppet-agent=1.0.0-1trusty
 
 # Change setting to allow Puppet agent service to run
 echo "***LOG:*** Changing setting to allow Puppet agent service to run"
@@ -42,11 +36,11 @@ sudo sed -i 's/START=no/START=yes/' /etc/default/puppet
 
 # Set name of PuppetMaster or use ga-puppet if not supplied
 echo "***LOG:*** Adding settings to puppet.conf"
-sudo puppet config set server ${PUPPETMASTER:-ga-puppet} --section agent
+sudo /opt/puppetlabs/bin/puppet config set server ${PUPPETMASTER:-ga-puppet} --section agent
 
 # Start Puppet Agent service
 echo "***LOG:*** Starting Puppet agent service"
-sudo puppet resource service puppet ensure=running enable=true
+sudo /opt/puppetlabs/bin/puppet resource service puppet ensure=running enable=true
 
 # Script complete
 echo "===SCRIPT COMPLETED==="
