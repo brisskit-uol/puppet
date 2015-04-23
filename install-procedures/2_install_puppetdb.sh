e0ca21f7a03c8056921430a114fbe424d77e2767
russparsloe <russell.parsloe@4thlevelit.co.uk>
2015-04-23 18:20:19 +0100

    Updated PuppetDB install to support Puppet 4.0

--- install-procedures/2_install_puppetdb.sh
+++ install-procedures/2_install_puppetdb.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 #===========================================================================================#
 # Installs PuppetDB																			#
-#																							#
-# v0.1 - 26/03/2015 - RP - 	Initial release													#
-# v0.2 - 27/03/2015 - RP -	Made script more generic.										#
-#							Install specific PuppetDB version.								#
-#							Use Puppet commands where possible for broader OS compatibility	#
 #===========================================================================================#
 
 # Set a location to be used for temporary files and logs
@@ -21,8 +16,8 @@ exec >> $TMPDIR/install-$(date +"%F-%H%M%S").log 2>&1
 
 # Download and install package to add Puppet repository
 echo "***LOG:*** Downloading Puppet repository package"
-wget -P $TMPDIR https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-sudo dpkg -i $TMPDIR/puppetlabs-release-trusty.deb
+wget -P $TMPDIR http://apt.puppetlabs.com/puppetlabs-release-pc1-trusty.deb
+sudo dpkg -i $TMPDIR/puppetlabs-release-pc1-trusty.deb
 
 # Refresh repositories
 echo "***LOG:*** Refreshing repositories"
@@ -30,13 +25,13 @@ sudo apt-get update
 
 # Install PuppetDB
 echo "***LOG:*** Installing PuppetDB"
-sudo puppet module install puppetlabs-puppetdb
+sudo /opt/puppetlabs/bin/puppet module install puppetlabs-puppetdb
 
 # Configure server to act as PuppetDB
-# Listen on 0.0.0.0 (all). Assumes puppetmaster-passenger is used and the puppetmaster service is apache2
+# Listen on 0.0.0.0 (all)
 # Install Puppetmaster
 echo "***LOG:*** Applying manual Puppet manifest"
-sudo puppet apply -e "class { 'puppetdb': listen_address => '0.0.0.0', puppetdb_version => '2.3.1-1puppetlabs1', } class { 'puppetdb::master::config': puppet_service_name => 'apache2', }"
+sudo /opt/puppetlabs/bin/puppet apply -e "class { 'puppetdb': listen_address => '0.0.0.0', puppetdb_version => '2.3.3-1puppetlabs1', } class { 'puppetdb::master::config': puppet_service_name => 'puppetserver', }"
 
 # Script complete
 echo "===SCRIPT COMPLETE==="
